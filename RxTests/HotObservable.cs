38543ede44b1946dbc8949e1a9f59050c2b32811
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-09-22 00:48:10 +0100

    Add a test re a hot observable

---
+++ RxTests/HotObservable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace RxTests
+{
+    [TestFixture]
+    internal class HotObservable
+    {
+        [Test]
+        public void HotObservableTest()
+        {
+            var period = TimeSpan.FromMilliseconds(100);
+            var coldObservable = Observable.Interval(period);
+            var hotObservable = coldObservable.Publish();
+            hotObservable.Connect();
+
+            var values = new List<long>();
+
+            {
+                var cancellationTokenSource1 = new CancellationTokenSource();
+                hotObservable.Subscribe(
+                    v =>
+                    {
+                        values.Add(v);
+                        if (values.Count == 5)
+                        {
+                            cancellationTokenSource1.Cancel();
+                        }
+                    },
+                    cancellationTokenSource1.Token);
+                cancellationTokenSource1.Token.WaitHandle.WaitOne();
+            }
+
+            Thread.Sleep(4 * period.Milliseconds + period.Milliseconds / 2);
+
+            {
+                var cancellationTokenSource2 = new CancellationTokenSource();
+                hotObservable.Subscribe(
+                    v =>
+                    {
+                        values.Add(v);
+                        if (values.Count == 10)
+                        {
+                            cancellationTokenSource2.Cancel();
+                        }
+                    },
+                    cancellationTokenSource2.Token);
+                cancellationTokenSource2.Token.WaitHandle.WaitOne();
+            }
+
+            Assert.That(values.ToArray(), Is.EqualTo(new[] {0, 1, 2, 3, 4, 9, 10, 11, 12, 13}));
+        }
+    }
+}
