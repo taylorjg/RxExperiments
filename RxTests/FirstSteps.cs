85b825e2d74c61b332c4a7e5d2ae486dcd588366
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-09-21 21:43:45 +0100

    Initial commit

---
+++ RxTests/FirstSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace RxTests
+{
+    [TestFixture]
+    internal class FirstSteps
+    {
+        [Test]
+        public void SimpleTestUsingObservableRange()
+        {
+            var observable = Observable.Range(1, 5);
+            var values = new List<int>();
+            observable.Subscribe(values.Add);
+            Assert.That(values, Has.Count.EqualTo(5));
+        }
+
+        [Test]
+        public void SimpleTestUsingObservableTimer()
+        {
+            var dueTime = TimeSpan.FromMilliseconds(100);
+            var period = TimeSpan.FromMilliseconds(100);
+            var observable = Observable.Timer(dueTime, period).Timestamp();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            var values = new List<string>();
+
+            observable.Subscribe(
+                x =>
+                    {
+                        values.Add(x.ToString());
+                        if (values.Count == 5)
+                        {
+                            cancellationTokenSource.Cancel();
+                        }
+                    },
+                cancellationTokenSource.Token);
+
+            cancellationTokenSource.Token.WaitHandle.WaitOne();
+
+            Assert.That(values, Has.Count.EqualTo(5));
+        }
+    }
+}
